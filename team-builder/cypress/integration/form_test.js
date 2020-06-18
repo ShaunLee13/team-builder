5c19425b504b20e9ba7053d96571c419175f71a0
Shaun Lee <shaundlee.0103@gmail.com>
2020-06-18 16:32:27 -0400

    Added tests on form functionality

--- team-builder/cypress/integration/form_test.js
+++ team-builder/cypress/integration/form_test.js
@@ -1,3 +1,28 @@
-/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 
+describe('Inputs functioning', () => {
+    it('Can Navigate to Page', () => {
+        cy.visit('http://localhost:3000/')
+    })
+
+    it('Can Input a Member Name',() => {
+        cy.get('input[name=name]')
+        .type('Jim Bob')
+    })
+    
+    it('Can input a member Email', () => {
+        cy.get('input[name=email]')
+        .type('bobert@bob.com')
+    })
+
+    it('should select Role', () => {
+        cy.get('select[name=role]')
+        .select('Front-End')
+    })
+
+    it('Can Submit Form', () => {
+        cy.get('button.SubmitButton')
+        .click()
+    })
+})
