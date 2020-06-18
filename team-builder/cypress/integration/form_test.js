1ad7a894fe57e28c3cf554b02c80ec72d27bd09f
Shaun Lee <shaundlee.0103@gmail.com>
2020-06-18 16:37:17 -0400

    Added should statements to verify values

--- team-builder/cypress/integration/form_test.js
+++ team-builder/cypress/integration/form_test.js
@@ -9,16 +9,19 @@ describe('Inputs functioning', () => {
     it('Can Input a Member Name',() => {
         cy.get('input[name=name]')
         .type('Jim Bob')
+        .should('have.value', 'Jim Bob')
     })
     
     it('Can input a member Email', () => {
         cy.get('input[name=email]')
         .type('bobert@bob.com')
+        .should('have.value', 'bobert@bob.com')
     })
 
     it('should select Role', () => {
         cy.get('select[name=role]')
         .select('Front-End')
+        .should('have.value', 'Front-End Engineer')
     })
 
     it('Can Submit Form', () => {
